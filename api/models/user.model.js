832ee5b6f18f9517b30306e99bf3aff5f0b2eff8
knightk3 <vyaskush016@gmail.com>
2024-04-13 11:41:49 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+    username: {
+        type: String,
+        required: true,
+        unqiue: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unqiue: true,
+    },
+    password:{
+        type:String,
+        required:true,
+
+    },
+    },{timestamps: true}
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
