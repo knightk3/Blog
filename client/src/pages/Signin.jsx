8a47817e1f1cc557318c791464355af9f1684680
knightk3 <vyaskush016@gmail.com>
2024-04-15 23:41:24 +0530

    add OAuth

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -7,6 +7,7 @@ import {
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
 
@@ -89,7 +90,8 @@ export default function SignIn() {
               ) : (
                 'Sign In'
               )}
-            </Button>  
+            </Button>
+            <OAuth/>  
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Dont Have an account?</span>
