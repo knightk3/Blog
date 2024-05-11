b58758f07b254fc4af8483689643c0e07f5f9b89
knightk3 <vyaskush016@gmail.com>
2024-05-11 16:21:55 +0530

    last commit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
             <p>
               On this blog, you will find weekly articles and tutorials on topics
               such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
+              languages. He is always learning and exploring new
               technologies, so be sure to check back often for new content!
             </p>
 
